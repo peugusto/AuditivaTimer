e7517ec89f8bfdb59094abe1926b042b56654ff9
Pedro Augusto <pedrooknd@gmail.com>
2025-05-11 02:49:29 -0300

    ..

--- Auditiva/src/App.jsx
+++ Auditiva/src/App.jsx
@@ -4,7 +4,7 @@ import ToggleTimer from './components/toggleTimer'
 import TopBar from './components/TopBar'
 import './App.css'
 import ToDo from './components/ToDo'
-import Joyride from 'react-joyride';
+
 
 
 function reducer(state,action){
@@ -23,32 +23,9 @@ function App() {
   const [timer,setTimer] = useState(true);
   const [tasksDone, setTasksDone] = useState(0);
   const [state, dispatch] = useReducer(reducer, {tarefas: [], count: 0})
-  const [run, setRun] = useState(true);
-
-  const steps = [
-  {
-    target: '.timer',
-    content: 'Este é o timer, ele permite que você gerencie seu tempo de foco e descanso de maneira eficiente.',
-  }, 
-  {
-    target: '.TopBar',
-    content: 'A TopBar exibe o número de tarefas concluídas, ajudando a visualizar seu desempenho.',
-  },
-  {
-    target: '.ToDo',
-    content: 'Aqui está a sua lista de tarefas. Você pode adicionar novos itens e marcar como concluídos à medida que avança.',
-  },
-];
 
   return (
     <>
-    <Joyride
-        steps={steps}
-        run={run}
-        continuous
-        showSkipButton
-        showProgress
-      />
     <div className='content'>
     <ToDo setTasksDone={setTasksDone} state={state} dispatch={dispatch}/>
     <div className='contentBlock'>
